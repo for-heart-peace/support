67e0f5932956a0a0c4527daf087593582890df4a
Lingxin Cao <957311501@qq.com>
2020-01-16 16:48:34 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,17 +6,20 @@
 int main()
 {
 	support_point test;
-	test.min_radius = 0.3;
+	test.min_radius = 0.25;
 	test.max_radius = 1.0;
-	test.top = 0.22;
-	test.cone_length = 0.5;
+	test.top = 0.3;
+	test.normal_top=0.3;
+	test.normal_cone_length=0.9;
+	test.normal_cyline_length = 0.2;
+	test.cone_length = 0.9;
 	test.bottom_radius = 8.0;
 	test.bottom_height = 0.5;
 	test.support_angle = 45.0;
 	test.to_mesh_angle = 55.0;
 	test.bottom_down = 1.5;
-	test.dis_x = 1.2;
-	test.dis_y = 1.2;
+	test.dis_x = 2.0;
+	test.dis_y = 2.0;
 	test.min_z = 100000.0;
 	test.min_x = 100000.0;
 	test.min_y = 100000.0;
@@ -29,5 +32,5 @@ int main()
 	test.GetSupportPoint();
 	finish=clock();
 	cout<<(finish-start)/CLOCKS_PER_SEC<<endl;
-
+	//getchar();
 }
